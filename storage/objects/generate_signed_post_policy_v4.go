9a2edf2ea1af25d64675a1f019f7cada060df02d
Frank Natividad <frankyn@users.noreply.github.com>
2020-05-14 10:58:17 -0700

    Update generate_signed_post_policy_v4.go

--- storage/objects/generate_signed_post_policy_v4.go
+++ storage/objects/generate_signed_post_policy_v4.go
@@ -32,6 +32,7 @@ var form = `<form action="{{ .URL }}" method="POST" enctype="multipart/form-data
 	{{- range $name, $value := .Fields }}
 	<input name="{{ $name }}" value="{{ $value }}" type="hidden"/>
 	{{- end }}
+
 	<input type="file" name="file"/><br />
 	<input type="submit" value="Upload File" name="submit"/><br />
 </form>`
